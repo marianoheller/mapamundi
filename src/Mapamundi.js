100adb8e7cc9f77c4dfbb9f3f3a733e939c8aee4
Mariano Mariano <marianofheller@gmail.com>
2017-12-12 09:35:10 -0300

    finished

--- src/Mapamundi.js
+++ src/Mapamundi.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import * as d3GeoProjection from 'd3-geo-projection';
 import * as topojson from 'topojson';
-import worldData from 'world-atlas/world/110m.json';
+import world from 'world-atlas/world/110m.json';
 
+import './Mapamundi.css';
 
 
 export default class Mapamundi extends Component {
@@ -13,27 +15,64 @@ export default class Mapamundi extends Component {
 
     drawMap() {
 
-        const svg = d3.select("#mapContainer")
-        .append("svg")
-        .attr("height", 400)
-        .attr("width", 600);
-        const path = d3.geoPath();
-
-        svg.append("g")
-        .attr("class", "countries")
-        .selectAll("path")
-        .data(topojson.feature(worldData, worldData.objects.countries).features)
-        .enter().append("path")
-            .attr("d", path);
+        var width = 960,
+        height = 580;
     
+        var color = d3.scaleOrdinal(d3.schemeCategory10);        
+        
+        var projection = d3GeoProjection.geoKavrayskiy7()
+            .scale(170)
+            .translate([width / 2, height / 2])
+            .precision(.1);
+
+        var path = d3.geoPath()
+            .projection(projection);
+        
+        var graticule = d3.geoGraticule();
+        
+        var svg = d3.select("#mapContainer").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+        
+        svg.append("defs").append("path")
+            .datum({type: "Sphere"})
+            .attr("id", "sphere")
+            .attr("d", path);
+        
+        svg.append("use")
+            .attr("class", "stroke")
+            .attr("xlink:href", "#sphere");
+        
+        svg.append("use")
+            .attr("class", "fill")
+            .attr("xlink:href", "#sphere");
+        
         svg.append("path")
-            .attr("class", "country-borders")
-            .attr("d", path(topojson.mesh(worldData, worldData.objects.countries, function(a, b) { return a !== b; })));
+            .datum(graticule)
+            .attr("class", "graticule")
+            .attr("d", path);
+        
+        var countries = topojson.feature(world, world.objects.countries).features,
+            neighbors = topojson.neighbors(world.objects.countries.geometries);
+        
+        svg.selectAll(".country")
+            .data(countries)
+            .enter().insert("path", ".graticule")
+            .attr("class", "country")
+            .attr("d", path)
+            .style("fill", function(d, i) { return color(d.color = d3.max(neighbors[i], function(n) { return countries[n].color; }) + 1 | 0); });
+        
+        svg.insert("path", ".graticule")
+            .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+            .attr("class", "boundary")
+            .attr("d", path);
+        
+        d3.select(window.frameElement).style("height", height + "px");
         
     }
 
     render() {
-        console.log(worldData);
+
         return (
             <div id="mapContainer"></div>
         )
